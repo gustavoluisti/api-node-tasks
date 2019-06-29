ec672ee1acfe014ebab1f5ad426293df80c8aa20
Luis Gustavo Santos <spartadev@spartas-MacBook-Pro.local>
2019-06-29 13:28:56 -0300

    Criando tabelas banco de dados com knex

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Meu Backend!')
